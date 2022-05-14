272a5c5c9ad4f66affe700c4d29c89985a7b26d6
afruchdman <alejandro.fruchdman@gmail.com>
2022-05-14 16:08:07 -0300

    Desafío: Menú e-commerce

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,15 +3,14 @@ import ItemListContainer from "./ItemListContainer.jsx";
 const Body = () => {
         return (
         <>
-      <ItemListContainer mesage="Hola Laura!"/>
-      <div class="container-xxl">
-        <div class="row">
-          <div class="col-xxl">
-              <img src="https://i.picsum.photos/id/1041/5184/2916.jpg?hmac=TW_9o6HeD7H7I7NVo-S1Fa1iAvzQ10uvmJqsXvNoi0M" alt="imagen"/>
+          <ItemListContainer mesage="Hola Laura!"/>
+          <div class="container-xxl">
+            <div class="row">
+              <div class="col-xxl">
+                  <img src="https://i.picsum.photos/id/1041/5184/2916.jpg?hmac=TW_9o6HeD7H7I7NVo-S1Fa1iAvzQ10uvmJqsXvNoi0M" alt="imagen"/>
+              </div>
+            </div>
           </div>
-        </div>
-
-      </div>
         </>
         );
     }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,9 @@ import CartWidget from "./CartWidget.jsx";
 const Navbar = () => {
         return (
         <>
-
-
-
           <nav className="navbar navbar-expand-lg navbar-light bg-success bg-gradient ">
             <div className="container-fluid  ">
               <a className="navbar-brand" href="../index.js">FRUCHDMAN</a>
-
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
@@ -35,8 +31,6 @@ const Navbar = () => {
               </div>
             </div>
           </nav>
-
-
         </>
         );
     }
